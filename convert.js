e6330a22f23e67f8b5c916273935e750ee4d217c
Carlton-08xx <73480883+Carlton-08xx@users.noreply.github.com>
2020-12-01 20:15:59 -0600

    update 777

--- convert.js
+++ convert.js
@@ -1,12 +1,26 @@
 function toFeet() { 
-    document.getElementById("feetValue").value = document.getElementById("milesValue").value * 5280; 
+    let feet = document.getElementById("feetValue") 
+    feet.value = document.getElementById("milesValue").value * 5280;
+    validate777() 
 }
 function toMiles() { 
     document.getElementById("milesValue").value = document.getElementById("feetValue").value / 5280; 
+    // if (document.getElementById("feetValue").value == "777") {
+    //     document.getElementById("message").innerHTML = "LUCKY!"
+    // }
+    // else {
+    //     document.getElementById("message").innerHTML = ""
+    // }
+    validate777()
+}
+
+function validate777() {
+    
     if (document.getElementById("feetValue").value == "777") {
         document.getElementById("message").innerHTML = "LUCKY!"
     }
     else {
         document.getElementById("message").innerHTML = ""
     }
-}
+
+} 
